e8bfe65022af31fcd4d6475e5b9f5ed2567afe03
rosahaj <141790572+rosahaj@users.noreply.github.com>
2024-05-04 14:22:20 +0200

    switch to undici (cjs-compliant)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { js2xml } from 'xml-js'
 import type { Options } from 'ky'
 import ky from 'ky'
+import { request } from 'undici'
 import { SimpleFaxError } from './errors.js'
 import type { EnvelopeOptions } from './types.js'
 import template from './template.json'
@@ -30,26 +31,20 @@ export function createEnvelope(options: EnvelopeOptions): string {
  * Throws a `SimpleFaxError` if the simple-fax.de server responds with an error
  *
  * @param options API options
- * @param kyOptions Additional request options passed to ky
  * @returns FaxId
  */
 export async function sendFax(
   options: EnvelopeOptions,
-  kyOptions?: Options,
 ): Promise<string> {
   const envelope = createEnvelope(options)
 
-  const response = await ky
-    .post('http://longisland.simple-fax.de/soap/index.php', {
-      ...kyOptions,
-      body: envelope,
-      headers: {
-        ...kyOptions?.headers,
-        'Content-Type': 'text/xml;charset=UTF-8',
-        'SOAPAction': `"urn:soapservice#Service#sendfax"`,
-      },
-    })
-    .text()
+  const response = await request('http://longisland.simple-fax.de/soap/index.php', {
+    body: envelope,
+    headers: {
+      'Content-Type': 'text/xml;charset=UTF-8',
+      'SOAPAction': `"urn:soapservice#Service#sendfax"`,
+    },
+  }).then(({ body }) => body.text())
 
   // Response format: "status;message."
   const [status, message] = response.trim().replace(/\.$/, '').split(';')
